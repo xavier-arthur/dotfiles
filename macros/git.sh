5ba16e0787bc129d0d17d053c00f79a7134f1d34
Arthur Xavier <garok102@gmail.com>
2022-10-23 13:21:12 -0300

    Adding macros
    bought a custom board im trying some new stuff

---
+++ macros/git.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Macro which checks if gitkraken is open and if so, focus on it, otherwise it just opens the program
+
+if ! dpkg -S wmctrl > /dev/null ; then
+    notify-send 'Required dependacy wmctrl not installed, macro quitting...'
+fi
+
+open_string=$(wmctrl -l | grep -i 'gitkraken' | awk '{ print $4 }')
+
+if [ ${#open_string} -gt 0 ]; then
+    wmctrl -a "${open_string}"
+else
+    gitkraken
+fi
