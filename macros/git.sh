b4100d972e2211273cfa60c0a475857ffa463787
Arthur Xavier <garok102@gmail.com>
2022-10-23 18:14:49 -0300

    optimized git macro a bit

--- macros/git.sh
+++ macros/git.sh
@@ -2,9 +2,10 @@
 
 # Macro which checks if gitkraken is open and if so, focus on it, otherwise it just opens the program
 
-if ! dpkg -S wmctrl > /dev/null ; then
-    notify-send 'Required dependacy wmctrl not installed, macro quitting...'
-fi
+# The block below makes the script super slow
+# if ! dpkg -S wmctrl > /dev/null ; then
+#     notify-send 'Required dependacy wmctrl not installed, macro quitting...'
+# fi
 
 open_string=$(wmctrl -l | grep -i 'gitkraken' | awk '{ print $4 }')
 
